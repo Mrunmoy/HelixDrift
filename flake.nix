e492f64d08b8f3d9a5417fc642b4944f62165a47
mrunmoy <mrunmoy@gmail.com>
2026-03-29 17:19:49 +1100

    build: restore nix flake on main

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "HelixDrift nRF mocap node workspace";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          python3
+          curl
+          cmake
+          ninja
+          gcc
+          gcc-arm-embedded
+        ];
+      };
+    };
+}
