740fa3289cb09842b5d8d935df5e0b88bcf07d62
mrunmoy <mrunmoy@gmail.com>
2026-04-02 05:32:23 +1100

    build: add nrf52dk bring-up target

--- examples/nrf52-blinky/src/main.cpp
+++ examples/nrf52-blinky/src/main.cpp
@@ -4,8 +4,26 @@
 #include <cstdint>
 
 namespace {
-constexpr uint32_t kLedPin = 13;
+#ifndef HELIX_LED_PIN
+#define HELIX_LED_PIN 13
+#endif
+
+#ifndef HELIX_LED_ACTIVE_LOW
+#define HELIX_LED_ACTIVE_LOW 0
+#endif
+
+constexpr uint32_t kLedPin = HELIX_LED_PIN;
 constexpr uint32_t kTickMs = 10;
+
+void driveLed(bool on) {
+#if HELIX_LED_ACTIVE_LOW
+    if (on) nrf_gpio_pin_clear(kLedPin);
+    else nrf_gpio_pin_set(kLedPin);
+#else
+    if (on) nrf_gpio_pin_set(kLedPin);
+    else nrf_gpio_pin_clear(kLedPin);
+#endif
+}
 }
 
 int main() {
@@ -13,15 +31,13 @@ int main() {
 
     helix::BlinkEngine blink(500, 500, true);
     bool level = blink.level();
-    if (level) nrf_gpio_pin_set(kLedPin);
-    else nrf_gpio_pin_clear(kLedPin);
+    driveLed(level);
 
     while (true) {
         const bool newLevel = blink.tick(kTickMs);
         if (newLevel != level) {
             level = newLevel;
-            if (level) nrf_gpio_pin_set(kLedPin);
-            else nrf_gpio_pin_clear(kLedPin);
+            driveLed(level);
         }
         nrf_delay_ms(kTickMs);
     }
